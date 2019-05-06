20c62903a7ff050cba1e6b07cf757c2eedf4a3a0
Kate <khummle@gmail.com>
2019-05-06 23:33:42 +0000

    creating a module and adding the print to it

---
+++ testingModule.js
@@ -0,0 +1,22 @@
+var getHTML = require('./http-functions.js');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step5.html'
+};
+
+function printHTML (html) {
+    console.log(html);
+    console.log('Response stream complete.');
+    
+}
+
+function saveHTML (html) {
+    // callMyOtherFUNCTIONconsole.log(html);
+    console.log("");
+    console.log("saved")
+}
+
+
+getHTML(requestOptions, printHTML);
+getHTML(requestOptions, saveHTML);
